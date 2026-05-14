04706a3a3b67cbbd5e911cdae1e17ad8a0e95164
Devadigk <krithika.devadiga@unisys.com>
2026-05-15 00:34:53 +0530

    first commit

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,37 @@
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using Persistence;
+
+namespace API.Controllers;
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ActivitiesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(int id)
+        {
+            var activity = await _context.Activities.FindAsync(id);
+            if (activity == null)
+            {
+                return NotFound();
+            }
+            return activity;
+        }
+    }
---
+++ Persistence/AppDbContextFactory.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Persistence;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlite("Data Source=reactivities.db");
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
